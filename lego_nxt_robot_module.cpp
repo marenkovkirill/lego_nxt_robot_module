b709d6cabe266d1d230a3167d0ff80a2ace4f957
iskinmike <Myasnikov.mike@gmail.com>
2015-07-20 13:36:03 +0500

    new api

--- lego_nxt_robot_module.cpp
+++ lego_nxt_robot_module.cpp
@@ -386,7 +386,7 @@ void *LegoRobotModule::writePC(unsigned int *buffer_length) {
 	return NULL;
 }
 
-FunctionResult* LegoRobot::executeFunction(system_value functionId, void **args) {
+FunctionResult* LegoRobot::executeFunction(CommandMode mode, system_value functionId, void **args) {
 	if ((functionId < 1) || (functionId > COUNT_LEGO_FUNCTIONS)) {
 		return NULL;
 	}
@@ -688,7 +688,7 @@ LegoRobot::LegoRobot(std::string connection, bool allow_dynamic):
 	robot_index = singletoneBrick->createBrick(connection_c);
 
 	uniq_name = new char[40];
-	sprintf(uniq_name, "robot-%u", robot_index);
+	sprintf(uniq_name, "robot-%s", connection.c_str());
 
 	if (!allow_dynamic) {
 		if (!connect()) {
